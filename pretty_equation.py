2703e81e30075bc1a5cc59b2b604630447a728fe
AjayPal2008 <128708345+AjayPal2008@users.noreply.github.com>
2026-09-11 14:16:20 -0500

    Create pretty_equation.py

---
+++ pretty_equation.py
@@ -0,0 +1,35 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Micah Kadiri
+#               Benjamin Hatch
+#               Ajay Palanisamy
+#               Hudson Dobbs
+# Section:      508
+# Assignment:   Lab Topic 3 (Team)/(optional)
+# Date:         11 September 2026
+from operator import eq
+
+
+A = int(input("Please enter the coefficient A:"))
+B = int(input("Please enter the coefficient B:"))
+C = int(input("Please enter the coefficient C:"))
+
+equation = ""
+
+if A == -1: equation += "-x^2 "
+elif A == 1: equation += "x^2 "
+else: equation += str(A)+"x^2 "
+
+if B == -1: equation += "- x "
+elif B == 1: equation += "+ x "
+elif B>0: equation += "+ "+str(abs(B))+"x "
+else: equation += "- "+str(abs(B))+"x "
+
+if C >0: equation+= "+ "+str(C)
+else: equation += str(C)
+
+
+print("The quadratic equation is "+ equation)# Output of equation
+
