6381708548ebca0614db1c4ebea30f59744a65a2
benjaminhatch <benjaminhatch@tamu.edu>
2026-09-14 13:06:57 -0500

    Add assignment agreement and team details to make_change.py

--- make_change.py
+++ make_change.py
@@ -1,1 +1,12 @@
-
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Micah Kadiri
+#               Benjamin Hatch
+#               Ajay Palanisamy
+#               Hudson Dobbs
+# Section:      508
+# Assignment:   Lab Topic 4 (Team)
+# Date:         14 September 2026
+#
