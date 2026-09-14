baf5bbb9e5c8b8e5aa3296564e04f31495dd1592
hudsonrdobbs <hudsonrdobbs@tamu.edu>
2026-09-14 16:24:53 -0500

    Update boolean_expressions.py

--- boolean_expressions.py
+++ boolean_expressions.py
@@ -24,3 +24,14 @@ print("a or b or c: ",a or b or c)
 XOR1 = a!=b
 print("XOR:",XOR1)
 print((b==c) or (b==a) or (a==c) or not(a==b and b==c and c==False))
+############ Part D ############ 
+Complex_1 = (not (a and b) or (not c and b)) and (not b) or (not a and b and not c) or (a and not b)
+Complex_2 = (not ((b or not c) and (not a or not c))) or (not (c or not (b and c))) or (a and not c) and (not a or (a and b and c) or (a and ((b and not c) or (not b))))
+
+Simple_1 = (not b) or (not a and not c)
+Simple_2 = (a or ((not b) and c))
+
+print("Complex 1:", Complex_1)
+print("Simple 1:", Simple_1)
+print("Complex 2:", Complex_2)
+print("Simple 2:", Simple_2)
